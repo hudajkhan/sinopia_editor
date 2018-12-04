5256638e47f99551fefbde1f9c1f82c29063dee9
Jeremy Nelson <jermnelson@gmail.com>
2018-12-04 10:00:10 -0700

    Added Redux js stubs for InputLiteral

---
+++ src/actions/editor/index.js
@@ -0,0 +1,6 @@
+// Copyright 2018 Stanford University see Apache2.txt for license
+
+export const addRDFTriple = triple => ({
+  type: 'ADD_RDF_TRIPLE',
+  triple
+})
---
+++ src/containers/editor/index.js
@@ -0,0 +1,6 @@
+// Copyright 2018 Stanford University see Apache2.txt for license
+
+import { connect } from 'react-redux'
+import InputLiteral from '../../../components/editor/InputLiteral'
+
+export default connect()(InputLiteral)
